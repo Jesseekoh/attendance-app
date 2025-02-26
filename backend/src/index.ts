19d19c6df5f99907eef467a727b6784078b0a989
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-02-26 08:47:18 +0100

    add rate limiting and improve error handling in authentication routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import { rateLimit } from 'express-rate-limit';
 import { config } from 'dotenv';
 import authRoutes from './routes/authRoutes';
 import sequelize from './config/db';
 config();
 const app = express();
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 100,
+    standardHeaders: 'draft-8',
+    legacyHeaders: false,
+});
+
+app.use(limiter);
 
 try {
     sequelize.authenticate();
@@ -22,4 +32,19 @@ app.use('/auth', authRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log('Server listening on port', process.env.PORT);
+});
+
+process.on('SIGINT', () => {
+    console.log('Server is shutting down');
+    sequelize
+        .close()
+        .then(() => {
+            console.log('Database closed.');
+        })
+        .catch((error) => {
+            console.log('Error closing Database');
+        })
+        .finally(() => {
+            process.exit(0);
+        });
 });
--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,13 +1,12 @@
 import Router from 'express';
 import Student from '../models/Student';
-import { Error, Model, Op } from 'sequelize';
 import bcrypt from 'bcrypt';
 
 const router = Router();
 
 const saltRounds = 10;
 
-router.post('/register', async (req, res): Promise<any> => {
+router.post('/register', async (req, res) => {
     const { firstName, lastName, email, matricNumber, password } = req.body;
     try {
         let passwordHash = await bcrypt.hash(password, saltRounds);
@@ -20,28 +19,53 @@ router.post('/register', async (req, res): Promise<any> => {
             passwordHash,
             matricNumber,
         });
-        return res.status(200).json({ message: 'User created successfully' });
+        res.status(200).json({ message: 'User created successfully' });
+        return;
     } catch (error: any) {
         console.log('Error: ', error);
         if (error?.name == 'SequelizeUniqueConstraintError') {
-            return res.status(409).json({ message: 'User already exists' });
+            res.status(409).json({ message: 'User already exists' });
+            return;
         }
 
-        return res
-            .status(500)
-            .json({ message: 'Something went wrong. Please try again later' });
+        res.status(500).json({
+            message: 'Something went wrong. Please try again later',
+        });
+        return;
     }
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
     const { email, password } = req.body;
 
     if (!email || !password) {
         res.status(400).json({ message: 'Email and password required' });
         return;
     }
+    try {
+        const existingUser = await Student.findOne({ where: { email } });
 
-    res.send('Logged in');
+        if (!existingUser) {
+            res.status(404).json({ message: 'User does not exist' });
+            return;
+        }
+        bcrypt.compare(
+            password,
+            existingUser?.getDataValue('passwordHash'),
+            (err, result) => {
+                if (result) {
+                    res.status(200).json({ message: 'Log in successful' });
+                    return;
+                }
+
+                res.status(401).json({ message: 'Wrong email or password' });
+            }
+        );
+    } catch (error: any) {
+        res.status(500).json({
+            message: 'Something went wrong. Please try again later',
+        });
+    }
 });
 
 router.post('/logout', (req, res) => {
