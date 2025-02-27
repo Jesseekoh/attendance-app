af53c122ed3e820a340b2ceae47b39922846fa04
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-02-27 20:01:43 +0100

    add Course model with fields for id, code, title, and description

---
+++ backend/src/models/Course.ts
@@ -0,0 +1,29 @@
+import crypto from 'crypto';
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/db';
+
+console.log(sequelize.define);
+const Course = sequelize.define('Course', {
+    id: {
+        type: DataTypes.BLOB,
+        primaryKey: true,
+        unique: true,
+        defaultValue: () => crypto.randomBytes(16),
+    },
+    code: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+    },
+    title: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+    },
+
+    desc: {
+        type: DataTypes.TEXT,
+    },
+});
+
+export default Course;
