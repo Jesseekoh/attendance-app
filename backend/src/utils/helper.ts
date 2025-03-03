2de3156c28f1d6c90d16cdc79c30bc93867fb5d4
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-03-03 15:19:52 +0100

    feat: add ITokenPayload interface and update token generation functions to include user role

--- backend/src/utils/helper.ts
+++ backend/src/utils/helper.ts
@@ -1,29 +1,18 @@
 import jwt from 'jsonwebtoken';
-export function generateAccessToken({
-    email,
-    id,
-}: {
-    email: string;
-    id: string;
-}) {
+import { ITokenPayload } from '../types';
+export function generateAccessToken({ id, role }: ITokenPayload) {
     const secret = process.env.ACCESS_TOKEN_SECRET;
 
     if (!secret) {
         throw new Error('Access JWT secret is not defined');
     }
-    return jwt.sign({ email, id }, secret, { expiresIn: '15m' });
+    return jwt.sign({ id, role }, secret, { expiresIn: '15m' });
 }
 
-export function generateRefreshToken({
-    email,
-    id,
-}: {
-    email: string;
-    id: string;
-}) {
+export function generateRefreshToken({ id, role }: ITokenPayload) {
     const secret = process.env.REFRESH_TOKEN_SECRET;
     if (!secret) {
         throw new Error('Refresh JWT secret is not defined');
     }
-    return jwt.sign({ email, id }, secret);
+    return jwt.sign({ role, id }, secret);
 }
