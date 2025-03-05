6c16c13b1b299a2a2f2ae07646ffb7b6b798a232
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-03-05 01:45:45 +0100

    feat: enhance teacher course management by using sequelize for bulk creation and improving response handling

--- backend/src/controllers/teacherController.ts
+++ backend/src/controllers/teacherController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
-import { Course, Teacher } from '../models';
+import { Course, sequelize, Teacher } from '../models';
+import { Sequelize } from 'sequelize';
 
 export async function addTeacherCourses(req: Request, res: Response) {
     const { id } = req.user;
@@ -13,14 +14,20 @@ export async function addTeacherCourses(req: Request, res: Response) {
             CourseId,
         }));
         if (teacher) {
-            const teacherCourses = await Teacher.bulkCreate(taughtCourses, {
-                include: [courses],
-            });
+            const teacherCourses =
+                await sequelize.models.taught_courses.bulkCreate(
+                    taughtCourses,
+                    {
+                        updateOnDuplicate: ['CourseId', 'TeacherId'],
+                    }
+                );
 
             res.status(200).json({ message: 'Courses added Successfully' });
+        } else {
+            res.status(403).json({
+                message: 'Only teachers can teach coursess',
+            });
         }
-
-        res.status(403).json({ message: 'Only teachers can teach coursess' });
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: 'Something went wrong. Try again' });
@@ -34,12 +41,17 @@ export async function getTeacherCourses(req: Request, res: Response) {
         const teacher = await Teacher.findOne({ where: { id } });
         if (teacher) {
             // Get all the courses taught by a teacher
-            const courses = await Teacher.findOne({
+            const data = await Teacher.findOne({
                 where: { id },
                 include: {
                     model: Course,
+                    through: { attributes: [] },
                 },
             });
+
+            const courses: Course[] | undefined = data?.Courses;
+
+            res.status(200).json(courses);
         } else {
             res.status(404).json({ message: 'Teacher not found' });
         }
