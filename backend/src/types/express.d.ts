9f935f7fc5e9180bf650ae58a35e2547c0288edc
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-03-14 10:35:15 +0100

    feat: update Express Request type to use ITokenPayload for user property

--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -1,9 +1,10 @@
 import { Request } from 'express';
+import { ITokenPayload } from '.';
 
 declare global {
     namespace Express {
         interface Request {
-            user?: any; // or a custom User type
+            user?: ITokenPayload; // or a custom User type
         }
     }
 }
