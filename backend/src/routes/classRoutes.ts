e76676f9a0a9239baa4b0a6a317849f2f49e2e0d
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-03-18 14:30:05 +0100

    feat: add routes for class details retrieval, attendance marking, and updating class information

--- backend/src/routes/classRoutes.ts
+++ backend/src/routes/classRoutes.ts
@@ -3,9 +3,15 @@ import { authenticateToken } from '../middlewares/authMiddleware';
 import classController from '../controllers/classController';
 const router = Router();
 
-router.get('/:classId', classController.getClass);
-
 // create a class
 router.post('/', authenticateToken, classController.createClass);
+
+// get class details
+router.get('/:classId', classController.getClass);
+
+// attend class
+router.post('/:classId', authenticateToken, classController.markAttendance);
+// Update class details
+router.put('/:classId', authenticateToken, classController.updateClassDetails);
 
 export default router;
