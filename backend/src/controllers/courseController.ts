0555c31eb855d6207d19dec258fa3e3c40381200
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-03-18 14:33:20 +0100

    feat: add controller to retrieve all students enrolled in a course

---
+++ backend/src/controllers/courseController.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from 'express';
+import { sequelize, User } from '../models';
+import logger from '../utils/logger';
+
+// Gets all students taking a course
+async function getAllCourseStudents(req: Request, res: Response) {
+    const { id } = req.user;
+    const { courseId } = req.params;
+    try {
+        const user = await User.findByPk(id);
+        if (user?.getDataValue('role') === 'teacher') {
+            const query = await sequelize.query(
+                'SELECT * FROM "Enrollments"  enrollments INNER JOIN users on enrollments."StudentId" = users.id INNER JOIN "Students" on enrollments."StudentId" = "Students".id where "CourseId" = :courseId',
+                {
+                    replacements: {
+                        courseId,
+                    },
+                }
+            );
+            const students = await query[0].map((student: any) => ({
+                id: student.id,
+                firstName: student.firstName,
+                lastName: student.lastName,
+                email: student.email,
+                matricNumber: student.matricNumber,
+                level: student.level,
+                department: student.department,
+            }));
+
+            res.status(200).json({ message: 'Successful', data: students });
+        } else {
+            res.status(403).json({
+                message: 'Only teachers can access this route',
+            });
+        }
+    } catch (error) {
+        logger.error(error);
+        res.status(500).json({
+            message: 'An error occurred retrieving students',
+        });
+    }
+}
+
+export default { getAllCourseStudents };
