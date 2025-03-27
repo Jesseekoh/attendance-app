19a1ac007d68d19a99034089cba630b10f2ca254
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-03-27 16:27:27 +0100

    feat: add Swagger documentation for authentication and class routes with detailed request and response schemas

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,13 +6,13 @@ import { rateLimit } from 'express-rate-limit';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import {
-    authRoutes,
-    classRoutes,
-    courseRoutes,
-    studentRoutes,
-    teacherRoutes,
-    userRoutes,
-    venueRoutes,
+  authRoutes,
+  classRoutes,
+  courseRoutes,
+  studentRoutes,
+  teacherRoutes,
+  userRoutes,
+  venueRoutes,
 } from './routes';
 import { sequelize } from './models';
 import logger from './utils/logger';
@@ -21,21 +21,51 @@ import swaggerUi from 'swagger-ui-express';
 
 const app = express();
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    limit: 100,
-    standardHeaders: 'draft-8',
-    legacyHeaders: false,
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: 'draft-8',
+  legacyHeaders: false,
 });
 
 const swaggerOptions = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Attendance app',
-            version: '1.0.0',
-        },
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Attendance app',
+      version: '1.0.0',
     },
-    apis: ['./src/routes/*.js'],
+    components: {
+      schemas: {
+        Class: {
+          type: 'object',
+          properties: {
+            courseId: {
+              type: 'string',
+              description: 'The ID of the course the class is for',
+            },
+            venueId: {
+              type: 'string',
+              description: 'The ID of the venue where the class is to be held',
+            },
+            startTime: {
+              type: 'string',
+              format: 'date-time',
+              description: 'When the class is to start',
+            },
+            endTime: {
+              type: 'string',
+              format: 'date-time',
+              description: 'When the class is to end',
+            },
+          },
+        },
+      },
+    },
+  },
+  apis: ['./src/routes/*.ts'],
+  servers: [
+    { url: 'http://localhost:5000/api', description: 'Development server' },
+  ],
 };
 
 const swaggerDocs = swaggerJSDoc(swaggerOptions);
@@ -48,14 +78,14 @@ app.use(cookieParser());
 
 // test database connection
 try {
-    sequelize.authenticate();
-    logger.info('Database connected successfully');
-    sequelize
-        .sync({ alter: true })
-        .then(() => logger.info('Created Tables successfully'))
-        .catch((error) => logger.error('Error creating tables: ', error));
+  sequelize.authenticate();
+  logger.info('Database connected successfully');
+  sequelize
+    .sync({ alter: true })
+    .then(() => logger.info('Created Tables successfully'))
+    .catch((error) => logger.error('Error creating tables: ', error));
 } catch (error) {
-    logger.error('failed to connect to database: ', error);
+  logger.error('failed to connect to database: ', error);
 }
 
 app.use('/api/v1/auth', authRoutes);
@@ -66,17 +96,17 @@ app.use('/api/v1/teachers', teacherRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/venues', venueRoutes);
 app.listen(process.env.PORT, () => {
-    logger.info(`Server is listening on port ${process.env.PORT}`);
+  logger.info(`Server is listening on port ${process.env.PORT}`);
 });
 
 const shutdown = async () => {
-    logger.info('Shutting Down...');
-    try {
-        await sequelize.close();
-        logger.info('Database closed.');
-    } catch (error) {
-        logger.info('An error occured when closing the database: ', error);
-    }
-    process.exit(0);
+  logger.info('Shutting Down...');
+  try {
+    await sequelize.close();
+    logger.info('Database closed.');
+  } catch (error) {
+    logger.info('An error occured when closing the database: ', error);
+  }
+  process.exit(0);
 };
 process.on('SIGINT', shutdown);
--- backend/src/routes/classRoutes.ts
+++ backend/src/routes/classRoutes.ts
@@ -3,10 +3,69 @@ import { authenticateToken } from '../middlewares/authMiddleware';
 import classController from '../controllers/classController';
 const router = Router();
 
-// create a class
+/**
+ * @swagger
+ *  /api/v1/classes:
+ *    post:
+ *      summary: Create class
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                courseId:
+ *                  type: string
+ *                  description: The ID of the course the class if for
+ *                venueId:
+ *                  type: string
+ *                  description: The ID of the venue where the class is to be held
+ *                startTime:
+ *                  type: string
+ *                  description: When the class is to start
+ *                endTime:
+ *                  type: string
+ *                  description: When the class is to end
+ *      responses:
+ *        "200":
+ *          description: Class created successfully
+ *        403:
+ *          description: Only teachers can access this route
+ *        400:
+ *          description: Class already exists
+ */
 router.post('/', authenticateToken, classController.createClass);
 
-// get class details
+/**
+ * @swagger
+ *  /api/v1/classes/:classId:
+ *    get:
+ *      summary: Get class by specified ID
+ *      parameters:
+ *        - in: path
+ *          name: classId
+ *          schema:
+ *            type: string
+ *          required: true
+ *          description: UUID of class to get
+ *      responses:
+ *        200:
+ *          description: OK
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  success:
+ *                    type: boolean
+ *                  message:
+ *                    type: string
+ *                  data:
+ *                    type: object
+ *
+ *
+ */
 router.get('/:classId', classController.getClass);
 
 // attend class
