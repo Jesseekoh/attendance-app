1063de75b0fed0234c2b7d8cbf3810bbf61e26d0
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-04-10 02:34:57 +0100

    refactor: improve formatting and consistency in token generation functions

--- backend/src/utils/helper.ts
+++ backend/src/utils/helper.ts
@@ -1,18 +1,18 @@
 import jwt from 'jsonwebtoken';
 import { ITokenPayload } from '../types';
 export function generateAccessToken({ id, role }: ITokenPayload) {
-    const secret = process.env.ACCESS_TOKEN_SECRET;
+  const secret = process.env.ACCESS_TOKEN_SECRET;
 
-    if (!secret) {
-        throw new Error('Access JWT secret is not defined');
-    }
-    return jwt.sign({ id, role }, secret, { expiresIn: '15m' });
+  if (!secret) {
+    throw new Error('Access JWT secret is not defined');
+  }
+  return jwt.sign({ id, role }, secret, { expiresIn: '5m' });
 }
 
 export function generateRefreshToken({ id, role }: ITokenPayload) {
-    const secret = process.env.REFRESH_TOKEN_SECRET;
-    if (!secret) {
-        throw new Error('Refresh JWT secret is not defined');
-    }
-    return jwt.sign({ role, id }, secret);
+  const secret = process.env.REFRESH_TOKEN_SECRET;
+  if (!secret) {
+    throw new Error('Refresh JWT secret is not defined');
+  }
+  return jwt.sign({ role, id }, secret, { expiresIn: '7d' });
 }
