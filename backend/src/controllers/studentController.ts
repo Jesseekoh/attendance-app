38cb0389c8fc6dc0bb3ac615ca2bedea06d8af1a
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-04-10 02:33:21 +0100

    refactor: improve code readability and consistency in studentController functions

--- backend/src/controllers/studentController.ts
+++ backend/src/controllers/studentController.ts
@@ -6,84 +6,86 @@ import logger from '../utils/logger';
  * Enrolls a student in the specified courses
  */
 export async function enrollCourses(req: Request, res: Response) {
-    const { id } = req.user;
-    const { courses } = req.body;
-    try {
-        const user = await Student.findOne({ where: { id } });
-        if (user) {
-            const enrollments = courses.map((courseId: string) => ({
-                CourseId: courseId,
-                StudentId: id,
-            }));
-            await Enrollment.bulkCreate(enrollments, {
-                updateOnDuplicate: ['CourseId', 'StudentId'],
-            });
+  const { id } = req.user;
+  const { courses } = req.body;
+  try {
+    const user = await Student.findOne({ where: { id } });
+    if (user) {
+      const enrollments = courses.map((courseId: string) => ({
+        CourseId: courseId,
+        StudentId: id,
+      }));
+      await Enrollment.bulkCreate(enrollments, {
+        updateOnDuplicate: ['CourseId', 'StudentId'],
+      });
 
-            res.status(200).json({
-                success: true,
-                message: 'Successfully enrolled courses',
-            });
-        } else {
-            res.status(403).json({
-                success: false,
-                message: 'Only students can enroll courses',
-            });
-        }
-    } catch (error: any) {
-        logger.error('An error occured', error);
-        res.status(500).json({
-            success: false,
-            message: 'Something went wrong. Please try again',
-            error: error.message,
-        });
+      res.status(200).json({
+        success: true,
+        message: 'Successfully enrolled courses',
+      });
+    } else {
+      res.status(403).json({
+        success: false,
+        message: 'Only students can enroll courses',
+      });
     }
+  } catch (error: any) {
+    logger.error('An error occured', error);
+    res.status(500).json({
+      success: false,
+      message: 'Something went wrong. Please try again',
+      error: error.message,
+    });
+  }
 }
 
 export async function getStudentCourses(req: Request, res: Response) {
-    const { id } = req.user;
+  const { id } = req.user;
 
-    try {
-        const student = await Student.findOne({ where: { id } });
+  try {
+    const student = await Student.findOne({ where: { id } });
 
-        if (student) {
-            // get all courses a student is enrolled in
-            const data = await Student.findOne({
-                where: { id },
-                include: {
-                    model: Course,
-                    attributes: ['id', 'code', 'title'],
-                    through: { attributes: [] },
-                },
-            });
+    if (student) {
+      // get all courses a student is enrolled in
+      const data = await Student.findOne({
+        where: { id },
+        include: {
+          model: Course,
+          attributes: ['id', 'code', 'title'],
+          through: { attributes: [] },
+        },
+      });
 
-            const courses: Course[] | undefined = data?.Courses;
+      const courses: Course[] = data!.Courses;
+      console.log('Courses', courses);
 
-            if (courses) {
-                res.status(200).json({
-                    success: true,
-                    message: 'Successfully fetched data',
-                    data: { courses },
-                });
-            } else {
-                res.status(200).json({
-                    success: true,
-                    message: 'Student is enrolled in no courses',
-                });
-            }
-        } else {
-            // response if no student is found
-            res.status(404).json({
-                success: false,
-                message: 'Student not found',
-            });
-        }
-    } catch (error) {
-        logger.error('An error occured', error);
-        res.status(500).json({
-            success: false,
-            message: 'Something went wrong. Please try again',
+      if (courses.length !== 0) {
+        res.status(200).json({
+          success: true,
+          message: 'Successfully fetched data',
+          data: courses,
         });
+      } else {
+        res.status(200).json({
+          success: true,
+          data: [],
+          message: 'Student is enrolled in no courses',
+        });
+      }
+    } else {
+      // response if no student is found
+      res.status(404).json({
+        success: false,
+        message: 'Student not found',
+      });
     }
+  } catch (error) {
+    logger.error('An error occured', error);
+    res.status(500).json({
+      success: false,
+      message: 'Something went wrong. Please try again',
+    });
+  }
 }
 
 export default { enrollCourses, getStudentCourses };
