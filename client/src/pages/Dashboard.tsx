ed6f6c942ef58c7012d82ab01df61563ffbe7099
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-04-21 07:50:34 +0100

    feat: integrate DashboardStats component into Dashboard and clean up unused code

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -2,28 +2,19 @@ import { useState } from 'react';
 import clsx from 'clsx';
 import UpcomingClasses from '../components/UpcomingClasses';
 import RecentClasses from '../components/RecentClasses';
+import DashboardStats from '../components/DashboardStats';
+
 const Dashboard = () => {
   const [activeTab, setActiveTab] = useState('upcoming');
+  // const [attendancePercentage, setAttendancePercentage] = useState(0);
 
   return (
     <div className="py-4">
-      {/* <h3 className="text-2xl font-[Inter] font-bold">Dashboard</h3> */}
-      {/* <div className="card"></div> */}
-      <div className="flex shadow-md border-2 border-neutral/10 rounded-md items-center divide-x-3 divide-neutral/20">
-        <div className="px-4">
-          <div
-            className="radial-progress text-primary flex-1"
-            style={{ '--value': 70 } as React.CSSProperties}
-            aria-valuenow={70}
-            role="progressbar"
-          >
-            70%
-          </div>
-        </div>
-        <div className="stat flex-1">
-          <h3 className="stat-title">Classes attendend</h3>
-          <h1 className="stat-value">13</h1>
-          <h3 className="stat-title">of 20 classes</h3>
+      <DashboardStats />
+      <div className="card !px-4">
+        <div className="card-title">Ongoing classes</div>
+        <div className="card-body px-0">
+          <p>You have no ongoing classes...</p>
         </div>
       </div>
 
