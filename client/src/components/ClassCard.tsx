7e5b0a7d3507dd360f8e2fecf8829debc2a600b0
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-05-07 21:42:28 +0100

    fix: update ClassCard component styling and structure for improved readability

--- client/src/components/ClassCard.tsx
+++ client/src/components/ClassCard.tsx
@@ -9,72 +9,72 @@ interface ClassCardProps {
 }
 const ClassCard: React.FC<ClassCardProps> = ({ classInfo }) => {
   return (
-    <div>
-      <div className="card card-md shadow-lg !p-4 border-neutral/20 border-2">
-        <div className="card-title">
-          <h3 className="text-lg">{classInfo.course.title}</h3>
+    // <div>
+    <div className="card card-md shadow-md border-2 border-neutral/10 !p-4 ">
+      <div className="card-title">
+        <h3 className="text-lg">{classInfo.course.title}</h3>
+      </div>
+
+      <div className="flex flex-col gap-1.5 mb-4">
+        {/* <p>{data.data.course.desc}</p> */}
+        <div className="flex gap-2">
+          <BookOpen className="text-neutral/40" />
+          <p>{classInfo.course.code}</p>
+        </div>
+        <div className="flex gap-2">
+          <MapPin className="text-neutral/40" />
+          <p>{classInfo.Venue.name}</p>
+        </div>
+        <div className="flex gap-2">
+          <Calendar className="text-neutral/40" />
+          <p>
+            {new Date(classInfo.startTime).toLocaleDateString([], {
+              weekday: 'long',
+              day: '2-digit',
+              month: 'long',
+            })}
+          </p>
+        </div>
+        <div className="flex gap-2">
+          <Clock className="text-neutral/40" />
+          <p>
+            {new Date(classInfo.startTime).toLocaleTimeString([], {})} -{' '}
+            {new Date(classInfo.endTime).toLocaleTimeString()}
+          </p>
+        </div>
+        <div className="flex gap-2">
+          <User className="text-neutral/40" />
+          <p>
+            {classInfo.Teacher.User.firstName +
+              ' ' +
+              classInfo.Teacher.User.lastName}
+          </p>
         </div>
 
-        <div className="flex flex-col gap-1.5 mb-4">
-          {/* <p>{data.data.course.desc}</p> */}
-          <div className="flex gap-2">
-            <BookOpen className="text-neutral/40" />
-            <p>{classInfo.course.code}</p>
-          </div>
-          <div className="flex gap-2">
-            <MapPin className="text-neutral/40" />
-            <p>{classInfo.Venue.name}</p>
-          </div>
-          <div className="flex gap-2">
-            <Calendar className="text-neutral/40" />
-            <p>
-              {new Date(classInfo.startTime).toLocaleDateString([], {
-                weekday: 'long',
-                day: '2-digit',
-                month: 'long',
+        {classInfo.attended !== undefined && (
+          <div>
+            <div
+              className={clsx('badge badge-soft badge-outline', {
+                'badge-success': classInfo.attended,
+                'badge-error': !classInfo.attended,
               })}
-            </p>
-          </div>
-          <div className="flex gap-2">
-            <Clock className="text-neutral/40" />
-            <p>
-              {new Date(classInfo.startTime).toLocaleTimeString([], {})} -{' '}
-              {new Date(classInfo.endTime).toLocaleTimeString()}
-            </p>
-          </div>
-          <div className="flex gap-2">
-            <User className="text-neutral/40" />
-            <p>
-              {classInfo.Teacher.User.firstName +
-                ' ' +
-                classInfo.Teacher.User.lastName}
-            </p>
-          </div>
-
-          {classInfo.attended !== undefined && (
-            <div>
-              <div
-                className={clsx('badge badge-soft badge-outline', {
-                  'badge-success': classInfo.attended,
-                  'badge-error': !classInfo.attended,
-                })}
-              >
-                {classInfo.attended ? 'Attended' : 'Absent'}
-              </div>
+            >
+              {classInfo.attended ? 'Attended' : 'Absent'}
             </div>
-          )}
-        </div>
-        <div className="card-action">
-          <Link
-            to={'/classes/' + classInfo.id}
-            className="btn bg-neutral text-neutral-content !rounded-lg w-full"
-            type="button"
-          >
-            View Details
-          </Link>
-        </div>
+          </div>
+        )}
+      </div>
+      <div className="card-action">
+        <Link
+          to={'/classes/' + classInfo.id}
+          className="btn bg-neutral text-neutral-content !rounded-lg w-full"
+          type="button"
+        >
+          View Details
+        </Link>
       </div>
     </div>
+    // </div>
   );
 };
 
