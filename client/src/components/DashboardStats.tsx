06f8d928f22dc8eee6b39f1f4c2a35a8197b5631
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-06-03 00:18:21 +0100

    feat: add Admin, Student, and Teacher dashboard components; refactor DashboardStats import path

--- client/src/components/DashboardStats.tsx
+++ client/src/components/DashboardStats.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { api } from '../api/axiosClient';
+import { api } from '../lib/axiosClient';
 const DashboardStats = () => {
   const {
     data: studentStats,
---
+++ client/src/components/StudentDashboard.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import clsx from 'clsx';
+import UpcomingClasses from '../components/UpcomingClasses';
+import RecentClasses from '../components/RecentClasses';
+import DashboardStats from '../components/DashboardStats';
+const StudentDashboard = () => {
+  const [activeTab, setActiveTab] = useState('upcoming');
+  return (
+    <div className="py-4">
+      <DashboardStats />
+      <div className="card !px-4">
+        <div className="card-title">Ongoing classes</div>
+        <div className="card-body px-0">
+          <p>You have no ongoing classes...</p>
+        </div>
+      </div>
+
+      <div role="tablist" className="tabs tabs-lift mt-6">
+        <button
+          role="tab"
+          className={clsx('tab', activeTab === 'upcoming' && 'tab-active')}
+          onClick={(e) => {
+            e.preventDefault();
+            setActiveTab('upcoming');
+          }}
+        >
+          Upcoming classes
+        </button>
+
+        <button
+          role="tab"
+          className={clsx('tab', activeTab === 'recent' && 'tab-active')}
+          onClick={(e) => {
+            e.preventDefault();
+            setActiveTab('recent');
+          }}
+        >
+          Recent Classes
+        </button>
+      </div>
+      <div>{activeTab === 'upcoming' && <UpcomingClasses />}</div>
+      <div>{activeTab === 'recent' && <RecentClasses />}</div>
+    </div>
+  );
+};
+
+export default StudentDashboard;
