3b7938342810e269f16e66499ad60443ecc615d4
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-09-22 12:52:27 +0100

    refactor: format express.d.ts for improved readability and update react-router version in package.json and pnpm-lock.yaml

--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -2,11 +2,11 @@ import { Request } from 'express';
 import { ITokenPayload } from '.';
 
 declare global {
-    namespace Express {
-        interface Request {
-            user?: any; // or a custom User type
-        }
+  namespace Express {
+    interface Request {
+      user?: any; // or a custom User type
     }
+  }
 }
 
 export {};
