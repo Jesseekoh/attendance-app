dc92d0211e503ed87ac807e32aadc1cbaa7a42f9
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-09-22 15:30:51 +0100

    refactor: remove unused API call and loading states in StudentsList component

--- client/src/components/StudentsList.tsx
+++ client/src/components/StudentsList.tsx
@@ -1,27 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
-import { api } from '@/lib/axiosClient';
-import { Skeleton } from '@/components/ui/skeleton';
 const StudentsList = () => {
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ['all-students'],
-    queryFn: async () => {
-      const resp = await api.get('/');
-      return resp;
-    },
-  });
-  if (isLoading) {
-    return (
-      <div className="flex flex-col gap-4">
-        <Skeleton className="h-32 w-full" />
-        <Skeleton className="h-4 w-28" />
-        <Skeleton className="h-4 w-full" />
-        <Skeleton className="h-4 w-full" />
-      </div>
-    );
-  }
-  if (isError) {
-    return <div>Error loading students</div>;
-  }
   return <div>StudentsList</div>;
 };
 
